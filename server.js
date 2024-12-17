fd9125686e61c293844515441e8eb0aa272b48ce
rodroberto <roberto.rodriguez.dev1125@gmail.com>
2024-12-17 03:41:01 -0500

    fix: image upload max size fix

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/profiles', express.static(path.join(__dirname, 'public', 'profiles')));
 app.use('/backgrounds', express.static(path.join(__dirname, 'public', 'backgrounds')));
